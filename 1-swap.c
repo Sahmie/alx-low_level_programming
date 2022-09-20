108b4df7d7fa26f2dc17ccbef2073f767494563b
samuel nwoko <sahmienwoko@gmail.com>
2022-09-20 16:02:35 -0700

    fix

---
+++ 1-swap.c
@@ -0,0 +1,13 @@
+i#include "main.h"
+/**
+ * * swap_int - swaps the value of two integers
+ * * @a: is first integer
+ * * @b: is second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+	*a = d;
+	*b = c;
+}
