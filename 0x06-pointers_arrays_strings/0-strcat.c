aeceab076ab9ca42010ff87a700dc9103f28db82
samuel nwoko <sahmienwoko@gmail.com>
2022-09-22 07:17:38 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  *   * @src: the source string to append to @dest
  *    * @dest: the destiation string to be concatenated upon
  *     * Return:pointer to the resulting string
- *      */
+ */
 
 char *_strcat(char *dest, char *src)
 
